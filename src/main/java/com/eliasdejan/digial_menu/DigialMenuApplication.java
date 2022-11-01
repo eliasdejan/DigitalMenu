9166e0bbb948075d5dc87073390cf5ac835e5879
Elias Dejan <elias.dejan0@gmail.com>
2022-11-01 18:55:03 +0200

    Initial commit

---
+++ src/main/java/com/eliasdejan/digial_menu/DigialMenuApplication.java
@@ -0,0 +1,13 @@
+package com.eliasdejan.digial_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigialMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigialMenuApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/eliasdejan/digial_menu/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.eliasdejan.digial_menu.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
